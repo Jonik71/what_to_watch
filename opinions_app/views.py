7e30a0cb30763d56fba09e9cb7a62b080488221c
Evgeny Inzhuvatov <jonbelovo7@gmail.com>
2026-05-29 12:03:49 +0700

    finish

--- opinions_app/views.py
+++ opinions_app/views.py
@@ -5,30 +5,44 @@ from flask import abort, flash, redirect, render_template, url_for
 from . import app, db
 from .forms import OpinionForm
 from .models import Opinion
+from .dropbox import async_upload_files_to_dropbox
+
+
+def random_opinion():
+    quantity = Opinion.query.count()
+    if quantity:
+        offset_value = randrange(quantity)
+        opinion = Opinion.query.offset(offset_value).first()
+        return opinion
 
 
 @app.route('/')
 def index_view():
-    quantity = Opinion.query.count()
-    if not quantity:
+    opinion = random_opinion()
+    # Если random_opinion() вернула None, значит, в БД нет записей.
+    if opinion is None:
         abort(500)
-    offset_value = randrange(quantity)
-    opinion = Opinion.query.offset(offset_value).first()
     return render_template('opinion.html', opinion=opinion)
 
 
-@app.route('/add', methods=['GET', 'POST'])
-def add_opinion_view():
+@@app.route('/add', methods=['GET', 'POST'])
+# Допишите ключевое слово async к функции.
+async def add_opinion_view():
     form = OpinionForm()
     if form.validate_on_submit():
         text = form.text.data
         if Opinion.query.filter_by(text=text).first() is not None:
             flash('Такое мнение уже было оставлено ранее!')
             return render_template('add_opinion.html', form=form)
+        # Замените вызов синхронной функции на вызов асинхронной.
+        # Обязательно добавьте ключевое слово await,
+        # так как функция async_upload_files_to_dropbox() асинхронная.
+        urls = await async_upload_files_to_dropbox(form.images.data)
         opinion = Opinion(
             title=form.title.data,
             text=text,
-            source=form.source.data
+            source=form.source.data,
+            images=urls
         )
         db.session.add(opinion)
         db.session.commit()
